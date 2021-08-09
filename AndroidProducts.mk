bcab6023979fd06d3e5824333155a9c5947b16bb
Subas <77564932+subas57@users.noreply.github.com>
2021-08-09 08:26:04 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_RMX1805.mk
+    $(LOCAL_DIR)/dot_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_RMX1805-user \
-    corvus_RMX1805-userdebug \
-    corvus_RMX1805-eng
+    dot_RMX1805-user \
+    dot_RMX1805-userdebug \
+    dot_RMX1805-eng
